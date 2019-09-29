a2fc7bc2ca482238505fd6e102adbe174583b1e6
Bastiaan Terhorst <bastiaan@perceptor.nl>
2019-09-29 17:58:33 +0200

    blog tweaks

--- config.rb
+++ config.rb
@@ -32,7 +32,7 @@ activate :blog do |blog|
 
   blog.permalink = "lab/{title}.html"
   # Matcher for blog source files
-  # blog.sources = "{year}-{month}-{day}-{title}.html"
+  blog.sources = "lab/{year}-{month}-{day}-{title}.html"
   # blog.taglink = "tags/{tag}.html"
   # blog.layout = "layout"
   # blog.summary_separator = /(READMORE)/
